6354e7a9b913ef26c17b5237bc8c756e99a2adca
briantmorr <briantmorr@gmail.com>
2018-03-17 15:29:55 -0700

    render json objects to dom

--- includes/main.js
+++ includes/main.js
@@ -15,5 +15,40 @@ function readData(){
     $.ajax(ajaxOptions);
 }
 function dataReceivedFromServer( responseData){
-    console.log(responseData);
+    var allRows = [];
+    if(responseData.success){
+        console.log('success true')
+        for(var i = 0; i < responseData.data.length; i++){
+            allRows.push(renderOneItemOnPage(responseData.data[i]));
+        }
+    }
+    else{
+        console.log('successs false');
+    }
+}
+function renderOneItemOnPage(todoItem){
+    console.log(todoItem);
+    var listItem = $('<li>');
+    var title = $('<h2>',{
+        text:  todoItem.title.toUpperCase()
+    });
+    var descriptionH = $('<h4>',{
+        text: 'Description: '
+    });
+    var description = $('<p>',{
+        text: todoItem.description
+    });
+    var dueH = $('<h4>',{
+        text: 'Due: '
+    });
+    var dueDate= $('<p>',{
+        text: todoItem.due
+    });
+    var delButton = $('<button>',{
+        text:'delete'
+    });
+    listItem.append(title, descriptionH, description, dueH, dueDate, delButton)
+    console.log('li is', listItem);
+     $('ul').prepend(listItem);
+   return listItem;
 }
