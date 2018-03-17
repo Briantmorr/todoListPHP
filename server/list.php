d75ff0a37d61ccf2fd79050c35f49581e0718e64
briantmorr <briantmorr@gmail.com>
2018-03-17 15:36:09 -0700

    added json as php backend to load data

---
+++ server/list.php
@@ -0,0 +1,14 @@
+{
+    "success": true,
+    "data": [{
+		"title": "review emails",
+		"description": "review feedback on  my emails",
+		"due": "3/18",
+		"id": 1
+	}, {
+		"title": "do react protoypes",
+		"description": "do my two react component: functional components, and class components(stopwatch)also do bonus challenge",
+		"due": "3/18",
+		"id": 4
+	}]
+}  
