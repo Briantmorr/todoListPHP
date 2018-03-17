b33fde61e399c3d702086373a50c88eed9bad185
briantmorr <briantmorr@gmail.com>
2018-03-17 13:14:34 -0700

    index files and json object

---
+++ list.php
@@ -0,0 +1,15 @@
+// sample json file
+list = {
+    data[
+            {
+            title: 'review emails',
+            description:'review feedback on  my emails',
+            due: '3/18'               
+            },
+            {
+            title: 'do react protoypes',
+            description:'do my two react component: functional components, and class components(stopwatch)also do bonus challenge',
+            due: '3/18'               
+            }
+        ]
+}
